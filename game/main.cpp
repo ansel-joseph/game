0048a77be32a1099e139d78c45dff3c6550bdd75
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-24 23:32:42 +0530

    add: added border

--- game/main.cpp
+++ game/main.cpp
@@ -1,11 +1,11 @@
 ﻿#include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Snake Game");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Snake Game");
 
-    sf::RectangleShape snakeHead({ 20.f, 20.f });
+    sf::RectangleShape snakeHead({20.f, 20.f});
     snakeHead.setFillColor(sf::Color::Red);
-    snakeHead.setPosition({ 100.f, 100.f });
+    snakeHead.setPosition({100.f, 100.f});
 
     float speed = 3.f;
 
@@ -15,20 +15,31 @@ int main() {
                 window.close();
         }
 
-        // SFML 3.0 uses sf::Keyboard::Key::Left (and so on)
+        // Movement
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-            snakeHead.move(sf::Vector2f{ -speed, 0.f });
+            snakeHead.move(sf::Vector2f{-speed, 0.f});
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-            snakeHead.move(sf::Vector2f{ speed, 0.f });
+            snakeHead.move(sf::Vector2f{speed, 0.f});
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
-            snakeHead.move(sf::Vector2f{ 0.f, -speed });
+            snakeHead.move(sf::Vector2f{0.f, -speed});
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
-            snakeHead.move(sf::Vector2f{ 0.f, speed });
+            snakeHead.move(sf::Vector2f{0.f, speed});
         }
 
+        // ✅ Border Collision Check
+        sf::Vector2f pos = snakeHead.getPosition();
+
+        if (pos.x < 0) pos.x = 0;
+        if (pos.x > 800 - 20) pos.x = 800 - 20;
+        if (pos.y < 0) pos.y = 0;
+        if (pos.y > 600 - 20) pos.y = 600 - 20;
+
+        snakeHead.setPosition(pos);
+
+        // Drawing
         window.clear(sf::Color::Black);
         window.draw(snakeHead);
         window.display();
